29c2c31e5271b76b42c6e711eac14b398ea89e32
bisous1519 <anipap2@naver.com>
2023-01-31 00:31:43 +0900

    [FE] Feat: Profile page layout

--- frontend/blahblah/src/components/auth/ProfileImage.tsx
+++ frontend/blahblah/src/components/auth/ProfileImage.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { createAvatar } from "@dicebear/core";
 import { personas, pixelArt } from "@dicebear/collection";
 
-export default function ProfileImage() {
+export default function ProfileImage({ ...rest }): JSX.Element {
   const avatar = useSelector((state: RootState) => state.user.userData.avatar!);
 
   const dataUri = createAvatar(personas, {
@@ -17,6 +17,8 @@ export default function ProfileImage() {
       alt="Sample"
       src={dataUri}
       // sx={{ width: 56, height: 56 }}
+      sx={{ ...rest }}
     />
   );
 }
+
