cfd996c09e091faa76f090071a2b9fe18b88ba21
seoda0000 <seoda0000@gmail.com>
2023-02-02 18:01:01 +0900

    [FE] Feat: Fade Heart 애니메이션 구현

--- frontend/blahblah/src/pages/LandingPage.tsx
+++ frontend/blahblah/src/pages/LandingPage.tsx
@@ -1,5 +1,7 @@
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import TestAnimation from "../components/landing/gsapTest";
+// import LandingEffect from "../components/landing/LandingEffect";
 import NavBarLanding from "../components/layout/NavBarLanding";
 
 function LandingPage() {
@@ -8,6 +10,8 @@ function LandingPage() {
       <NavBarLanding />
 
       <h1>랜딩페이지</h1>
+      {/* <LandingEffect /> */}
+      <TestAnimation />
       <Link to="/main">
         <Button>Skip</Button>
       </Link>
