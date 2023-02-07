1856abacd8097806bd4743ac72ca65483b6e6353
Junewoo Byeon <movebxeax@gmail.com>
2023-02-07 10:05:16 +0900

    [FE] Fix: 튜토리얼페이지 faceapi, teachable machine 둘 다 사용가능하도록 수정

--- frontend/blahblah/src/pages/TutorialPage.tsx
+++ frontend/blahblah/src/pages/TutorialPage.tsx
@@ -1,10 +1,11 @@
-// import FaceExpressionRecognition from "../components/faceapi/FaceExpressionRecognition";
+import FaceExpressionRecognition from "../components/faceapi/FaceExpressionRecognition";
 import PoseRecognition from "../components/teachable-machine/PoseRecognition"
 
 function TutorialPage() {
   return (
     <div>
       <h1>튜토리얼페이지</h1>
+      <FaceExpressionRecognition />
       <PoseRecognition />
     </div>
   );
