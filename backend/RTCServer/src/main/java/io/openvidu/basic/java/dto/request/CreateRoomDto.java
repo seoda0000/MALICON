7ea120c67460a312a90b228de5091b324fc424ec
Junewoo Byeon <movebxeax@gmail.com>
2023-02-07 09:20:09 +0900

    [BE] fix: CreateRoomDto RequiredArgsConstructor 제거

--- backend/RTCServer/src/main/java/io/openvidu/basic/java/dto/request/CreateRoomDto.java
+++ backend/RTCServer/src/main/java/io/openvidu/basic/java/dto/request/CreateRoomDto.java
@@ -2,11 +2,9 @@ package io.openvidu.basic.java.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Builder
 @Getter
-@RequiredArgsConstructor
 public class CreateRoomDto {
     private String title;
     private String hashTag;
