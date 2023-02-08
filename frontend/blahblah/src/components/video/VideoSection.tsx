a47fd11be5121d93cde028c77286f07cfb0b4135
Dakyung Seo <seoda0000@gmail.com>
2023-02-08 16:00:42 +0900

    [FE] Feat: Video Comment 영역 추가

--- frontend/blahblah/src/components/video/VideoSection.tsx
+++ frontend/blahblah/src/components/video/VideoSection.tsx
@@ -14,6 +14,7 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CommentSection from "../feed/CommentSection";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -60,11 +61,10 @@ export default function VideoCard() {
         alt="Paella dish"
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
+        {/* <CommentSection
+          comments={}
+          articleId={}
+        /> */}
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
