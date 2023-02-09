11b2d4c1bf0e4ccc77a0ae9c28bb67482b399f94
bisous1519 <anipap2@naver.com>
2023-02-09 18:00:58 +0900

    [FE] Fix: 빌드에러 해결

--- frontend/blahblah/src/redux/modules/index.ts
+++ frontend/blahblah/src/redux/modules/index.ts
@@ -1,14 +1,12 @@
 import { combineReducers } from "redux";
 import user from "./user/slice";
 import profile from "./profile/slice";
-import subscribe from "./subscribe/slice";
 import video from "./video/video-slice";
 import feed from "./feed/feed-slice";
 import broadcast from "./broadcast/broadcast-slice";
 export const reducer = combineReducers({
   user,
   profile,
-  subscribe,
   feed,
   video,
   broadcast,
