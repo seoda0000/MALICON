4a836457b4bb7e88b6f0dbbe43dfdb57c20a3fb1
Dakyung Seo <seoda0000@gmail.com>
2023-02-09 13:38:43 +0900

    [FE] Feat: 비디오 해시태그 추가, Nav 상태 표시

--- frontend/blahblah/src/redux/modules/video/video-slice.ts
+++ frontend/blahblah/src/redux/modules/video/video-slice.ts
@@ -32,8 +32,7 @@ const initialState: VideoStateType = {
       views: 340,
       pathUrl: "동영상경로여2",
       createDate: "2023-02-06T16:58:06.093775",
-      hashtags:
-        '[{"key":6,"label":"재즈","selected":true},{"key":7,"label":"클래식","selected":true},{"key":11,"label":"레게","selected":true},{"key":12,"label":"댄스","selected":true}]',
+      hashtags: '[{"key":0,"label":"K-POP","selected":true}]',
       // comments: {
       //   content: [],
       // },
@@ -50,7 +49,7 @@ const initialState: VideoStateType = {
       pathUrl: "동영상경로여3",
       createDate: "2023-02-06T16:58:06.093775",
       hashtags:
-        '[{"key":6,"label":"재즈","selected":true},{"key":7,"label":"클래식","selected":true},{"key":11,"label":"레게","selected":true},{"key":12,"label":"댄스","selected":true}]',
+        '[{"key":3,"label":"트로트","selected":true},{"key":22,"label":"그룹","selected":true}]',
       // comments: {
       //   content: [],
       // },
@@ -69,7 +68,7 @@ const initialState: VideoStateType = {
       pathUrl: "구독동영상경로여1",
       createDate: "2023-02-06T16:58:06.093775",
       hashtags:
-        '[{"key":6,"label":"재즈","selected":true},{"key":7,"label":"클래식","selected":true},{"key":11,"label":"레게","selected":true},{"key":12,"label":"댄스","selected":true}]',
+        '[{"key":14,"label":"통기타","selected":true},{"key":16,"label":"밴드","selected":true}]',
       // comments: {
       //   content: [],
       // },
@@ -85,8 +84,7 @@ const initialState: VideoStateType = {
       views: 430,
       pathUrl: "동영상경로여",
       createDate: "2023-02-06T16:58:06.093775",
-      hashtags:
-        '[{"key":6,"label":"재즈","selected":true},{"key":7,"label":"클래식","selected":true},{"key":11,"label":"레게","selected":true},{"key":12,"label":"댄스","selected":true}]',
+      hashtags: '[{"key":13,"label":"EDM","selected":true}]',
       // comments: {
       //   content: [],
       // },
@@ -103,7 +101,7 @@ const initialState: VideoStateType = {
       pathUrl: "동영상경로여",
       createDate: "2023-02-06T16:58:06.093775",
       hashtags:
-        '[{"key":6,"label":"재즈","selected":true},{"key":7,"label":"클래식","selected":true},{"key":11,"label":"레게","selected":true},{"key":12,"label":"댄스","selected":true}]',
+        '[{"key":18,"label":"팬미팅","selected":true},{"key":17,"label":"리코더","selected":true}]',
       // comments: {
       //   content: [],
       // },
