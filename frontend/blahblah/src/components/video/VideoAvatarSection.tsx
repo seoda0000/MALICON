be8540afe110d2e6676187590203eaade31b7b7d
Dakyung Seo <seoda0000@gmail.com>
2023-02-11 22:44:08 +0900

    [FE] Feat: 시간에 따른 아바타 감정표현

--- frontend/blahblah/src/components/video/VideoAvatarSection.tsx
+++ frontend/blahblah/src/components/video/VideoAvatarSection.tsx
@@ -16,10 +16,17 @@ const SAMPLE_EMOTION_LOG = [
     userId: "ssafy1",
   },
   {
+    time: 5001,
+    avatar:
+      '{"body":["rounded"],"clothingColor":["54d7c7"],"eyes":["happy"],"facialHair":[""],"facialHairProbability":100,"hair":["curlyHighTop"],"hairColor":["6c4545"],"mouth":["bigSmile"],"nose":["smallRound"],"skinColor":["d78774"]}',
+    type: "happy",
+    userId: "ssafy2",
+  },
+  {
     time: 10000,
     avatar:
       '{"body":["rounded"],"clothingColor":["54d7c7"],"eyes":["glasses"],"facialHair":[""],"facialHairProbability":100,"hair":["curlyHighTop"],"hairColor":["6c4545"],"mouth":["smile"],"nose":["smallRound"],"skinColor":["d78774"]}',
-    type: "happy",
+    type: "sad",
     userId: "ssafy2",
   },
   {
@@ -40,7 +47,7 @@ const SAMPLE_EMOTION_LOG = [
     time: 25000,
     avatar:
       '{"body":["rounded"],"clothingColor":["54d7c7"],"eyes":["happy"],"facialHair":[""],"facialHairProbability":100,"hair":["curlyHighTop"],"hairColor":["6c4545"],"mouth":["bigSmile"],"nose":["smallRound"],"skinColor":["d78774"]}',
-    type: "sad",
+    type: "happy",
     userId: "ssafy1",
   },
 ];
@@ -48,10 +55,13 @@ const VideoAvatarSection: React.FC<{ played: number; emotionLog: any }> = (
   props
 ) => {
   let avatarList: UserAvatar[] = [];
+  let timeline: any[] = [];
+  let timeList: number[] = [];
 
   {
     SAMPLE_EMOTION_LOG &&
       SAMPLE_EMOTION_LOG.map((emotion: any) => {
+        // 아바타 생성
         let user: UserAvatar = {
           userId: emotion.userId,
           avatar: emotion.avatar,
@@ -60,32 +70,53 @@ const VideoAvatarSection: React.FC<{ played: number; emotionLog: any }> = (
         if (!avatarList.find((element) => element.userId === user.userId)) {
           avatarList.push(user);
         }
+
+        // 타임라인 생성
+        let time = Math.round(emotion.time / 1000);
+        if (timeList.indexOf(time) === -1) {
+          timeline.push({ time: time, user: [user] });
+          timeList.push(time);
+        } else {
+          timeline[timeline.length - 1].user.push(user);
+        }
       });
   }
 
-  console.log(avatarList);
-  const [emotionLog, setEmotionLog] = useState(
-    avatarList.map((elem) => {
-      return {
-        userId: elem.userId,
-        avatar: elem.avatar,
-        type: "happy",
-      };
-    })
-  );
+  let emotionLogFirst = avatarList.map((elem) => {
+    return {
+      userId: elem.userId,
+      avatar: elem.avatar,
+      type: "",
+    };
+  });
+  let emotionLogChange = avatarList.map((elem) => {
+    return {
+      userId: elem.userId,
+      avatar: elem.avatar,
+      type: "",
+    };
+  });
+  const [emotionLog, setEmotionLog] = useState(emotionLogFirst);
+
   useEffect(() => {
-    SAMPLE_EMOTION_LOG.map((emotion: any) => {
-      if (Math.round(emotion.time / 1000) === Math.round(props.played)) {
-        setEmotionLog(
-          avatarList.map((elem) => {
-            return {
-              userId: elem.userId,
-              avatar: elem.avatar,
-              type: emotion.type,
-            };
-          })
+    const findUserId = (val: string, array: UserAvatar[]) => {
+      for (const a of array) {
+        if (val === a.userId) {
+          return a.type;
+        }
+      }
+      return false;
+    };
+    timeline.map((item: any) => {
+      if (item.time === Math.round(props.played)) {
+        // console.log("해당시각 유저목록", item.user);
+        emotionLogChange = emotionLogChange.map((elem) =>
+          findUserId(elem.userId, item.user)
+            ? { ...elem, type: findUserId(elem.userId, item.user) as string }
+            : { ...elem, type: "" }
         );
-      } else {
+        setEmotionLog(emotionLogChange);
+        // console.log("업데이트");
         return;
       }
     });
