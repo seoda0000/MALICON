fb7fa40be50eeac438863543c79dd28dc21023ad
Junewoo Byeon <movebxeax@gmail.com>
2023-02-13 22:44:11 +0900

    [FE] Fix: 방송 시작시 사용자 정보 넘기도록 수정

--- frontend/blahblah/src/pages/BroadcastPage.tsx
+++ frontend/blahblah/src/pages/BroadcastPage.tsx
@@ -3,6 +3,8 @@ import registerServiceWorker from "../registerServiceWorker";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch } from "../redux/configStore";
 import { RootState } from "../redux/configStore";
+import { useState } from "react";
+import { useAppSelector } from "../redux/configStore.hooks";
 
 function BroadcastPage() {
   const dispatch = useDispatch<AppDispatch>();
@@ -10,7 +12,7 @@ function BroadcastPage() {
 
   return (
     <div>
-      <VideoRoomComponent sessionName={broadcast.currentSession.sessionId} />
+      <VideoRoomComponent sessionName={broadcast.currentSession.sessionId} user={useAppSelector((state) => state.user.userData)} />
       {/* <VideoRoomComponent /> */}
     </div>
   );
