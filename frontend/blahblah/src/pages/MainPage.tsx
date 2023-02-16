bb53a52f7fa70dcbd01c5c24a96c9f830ca0a830
Dakyung Seo <seoda0000@gmail.com>
2023-02-16 23:41:34 +0900

    [FE] Fix: 비디오 섹션 UI 개선

--- frontend/blahblah/src/pages/MainPage.tsx
+++ frontend/blahblah/src/pages/MainPage.tsx
@@ -172,6 +172,7 @@ function MainPage(): JSX.Element {
           sessions={broadcast.sessions}
           setOpenLoginAlert={setOpenLoginAlert}
         />
+
         {/* <h1>예시</h1> */}
         {/* <BroadcastCarousel sessions={SAMPLE_SESSIONS} /> */}
         {/* <h1>추천 동영상</h1> */}
@@ -182,6 +183,7 @@ function MainPage(): JSX.Element {
           handleClickAll={handleClickAll}
           isMain={true}
         />
+
         <VideoList chipList={chipList} setOpenLoginAlert={setOpenLoginAlert} />
       </div>
 
