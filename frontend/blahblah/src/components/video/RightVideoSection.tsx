c7f00680a944215b90784f01fcdbc222473becd9
Dakyung Seo <seoda0000@gmail.com>
2023-02-17 15:05:28 +0900

    [FE] Fix: 우측 sticky

--- frontend/blahblah/src/components/video/RightVideoSection.tsx
+++ frontend/blahblah/src/components/video/RightVideoSection.tsx
@@ -8,11 +8,23 @@ import { RootState } from "../../redux/configStore";
 import { AppDispatch } from "../../redux/configStore";
 import { fetchFollowingVideoData } from "../../redux/modules/video";
 import { useAppSelector } from "../../redux/configStore.hooks";
+import styled from "@emotion/styled";
 let isInitial = true;
+
+const RightVideoSectionContainer = styled.div`
+  width: 280px;
+  & > div {
+    position: fixed;
+    top: 20px;
+    right: 50px;
+    width: 280px;
+  }
+`;
+
 const RightVideoSection: React.FC<{ drawerWidth: number }> = (props) => {
   const video = useSelector((state: RootState) => state.video);
   const dispatch = useDispatch<AppDispatch>();
-  const isLoggedIn = useAppSelector((state) => state.user.userData.isLoggedIn)
+  const isLoggedIn = useAppSelector((state) => state.user.userData.isLoggedIn);
 
   useEffect(() => {
     if (isInitial) {
@@ -24,34 +36,46 @@ const RightVideoSection: React.FC<{ drawerWidth: number }> = (props) => {
   }, [video, dispatch]);
 
   return (
-    <Box sx={{ width: { md: props.drawerWidth }, flexShrink: { sm: 0 } }}>
-      <Paper
-        // variant="permanent"
-        sx={{
-          display: { xs: "none", md: "flex" },
-          "& .MuiDrawer-paper": {
-            boxSizing: "border-box",
-            width: props.drawerWidth,
-          },
-          ml: 5,
-          p: 2,
-        }}
-      >
-        {/* 컨텐츠 내용 */}
-        <Grid container rowSpacing={3}>
-          <h3>Following Video</h3>
-          {video.followingVideoList.length > 0 ? (
-            video.followingVideoList.slice(0, 4).map((video, index) => (
-              <Grid item width={"100%"} key={index}>
-                <VideoCard nth={3} video={video} />
-              </Grid>
-            ))
-          ) : isLoggedIn ? (
-            <div>팔로잉 중인 뮤지션이 없습니다. 팔로우 해주세요! </div>
-          ) : <div>팔로잉 목록을 표시하려면 로그인 해주세요!</div>}
-        </Grid>
-      </Paper>
-    </Box>
+    // <Box
+    //   sx={{
+    //     width: { md: props.drawerWidth },
+    //     flexShrink: { sm: 0 },
+    //     // position: "sticky",
+    //   }}
+    // >
+    <RightVideoSectionContainer id="thatthat">
+      <div>
+        <Paper
+          // variant="permanent"
+          sx={{
+            display: { xs: "none", md: "flex" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: props.drawerWidth,
+            },
+            ml: 5,
+            p: 2,
+          }}
+        >
+          {/* 컨텐츠 내용 */}
+          <Grid container rowSpacing={3}>
+            {/* <h3>Following Video</h3> */}
+            {video.followingVideoList.length > 0 ? (
+              video.followingVideoList.slice(0, 3).map((video, index) => (
+                <Grid item width={"100%"} key={index}>
+                  <VideoCard nth={3} video={video} />
+                </Grid>
+              ))
+            ) : isLoggedIn ? (
+              <div>팔로잉 중인 뮤지션이 없습니다. 팔로우 해주세요! </div>
+            ) : (
+              <div>팔로잉 목록을 표시하려면 로그인 해주세요!</div>
+            )}
+          </Grid>
+        </Paper>
+        {/* </Box> */}
+      </div>
+    </RightVideoSectionContainer>
   );
 };
 
